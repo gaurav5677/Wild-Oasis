77f883a9073f20318e02537196cb48ff31717a59
Gaurav <gauravpatilfamily7@gmail.com>
2023-08-30 11:12:38 +0530

    Styled Button and Imported Font-family

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,8 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyle";
+import Button from "./ui/Button";
+import Input from "./ui/input";
+
+const H1 = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orange;
+  padding: 20px;
+`;
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("click in ")}> Click here</Button>
+        <Button onClick={() => alert("click in ")}> Click here</Button>
+
+        <Input type="number" placeholder="number of guests " />
+        <Input type="number" placeholder="number of guests " />
+      </StyledApp>
+    </>
   );
 }
 
