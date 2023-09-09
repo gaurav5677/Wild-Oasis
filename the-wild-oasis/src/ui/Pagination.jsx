80da5c37bc8441309377632a841a2719bf6945cd
Gaurav <gauravpatilfamily7@gmail.com>
2023-09-09 19:59:37 +0530

    Build Reusable Pagination Component

--- the-wild-oasis/src/ui/Pagination.jsx
+++ the-wild-oasis/src/ui/Pagination.jsx
@@ -60,6 +60,7 @@ const PaginationButton = styled.button`
 `;
 
 const PAGE_SIZE = 10;
+
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -69,22 +70,40 @@ function Pagination({ count }) {
 
   const pageCount = Math.ceil(count / PAGE_SIZE);
 
-  function nextPage() {}
-  function prevPage() {}
+  function nextPage() {
+    const next = currentPage === pageCount ? currentPage : currentPage + 1;
 
+    searchParams.set("page", next);
+    setSearchParams(searchParams);
+  }
+  function prevPage() {
+    const prev = currentPage === 1 ? currentPage : currentPage - 1;
+
+    searchParams.set("page", prev);
+    setSearchParams(searchParams);
+  }
+  if (pageCount <= 1) return null;
   return (
     <StyledPagination>
       <P>
         Showing
-        <span> 1 </span> to <span> 10 </span> of <span> {count} </span> results
+        <span> {(currentPage - 1) * PAGE_SIZE + 1} </span> to{" "}
+        <span>
+          {" "}
+          {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
+        </span>{" "}
+        of <span> {count} </span> results
       </P>
 
       <Buttons>
-        <PaginationButton onClick={prevPage}>
+        <PaginationButton onClick={prevPage} disabled={currentPage === 1}>
           <HiChevronLeft /> <span> Previous</span>
         </PaginationButton>
 
-        <PaginationButton onClick={nextPage}>
+        <PaginationButton
+          onClick={nextPage}
+          disabled={currentPage === pageCount}
+        >
           <HiChevronRight /> <span> Next</span>
         </PaginationButton>
       </Buttons>
