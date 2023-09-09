777d1cef8bc15262bf31e55a58f379b0dce26053
Gaurav <gauravpatilfamily7@gmail.com>
2023-09-09 23:10:31 +0530

    Paginating Bookings page

--- the-wild-oasis/src/ui/Pagination.jsx
+++ the-wild-oasis/src/ui/Pagination.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../utils/constants";
 /* eslint react/prop-types: 0 */
 
 const StyledPagination = styled.div`
@@ -59,7 +60,7 @@ const PaginationButton = styled.button`
   }
 `;
 
-const PAGE_SIZE = 10;
+// const PAGE_SIZE = 10;
 
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
