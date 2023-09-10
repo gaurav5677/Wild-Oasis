b65885619204d227d5529986c9d5f6abf6d91bbb
Gaurav <gauravpatilfamily7@gmail.com>
2023-09-10 11:31:29 +0530

    Implemented Prefetching   in booking page

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -35,6 +35,7 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingID" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
