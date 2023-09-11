402023e8130c5b4c2fa2d0853baf91ebdc6e5536
Gaurav <gauravpatilfamily7@gmail.com>
2023-09-11 20:40:57 +0530

    user Logout Implemented

--- the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -19,7 +19,14 @@ function LoginForm() {
 
     if (!email || !password) return;
 
-    login({ email, password });
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail(""), setPassword("");
+        },
+      }
+    );
   }
 
   return (
--- the-wild-oasis/src/ui/ProtectedRoute.jsx
+++ the-wild-oasis/src/ui/ProtectedRoute.jsx
@@ -1,5 +1,44 @@
+import Spinner from "../ui/Spinner";
+import { useUser } from "../features/authentication/useUser";
+import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+/* eslint react/prop-types: 0 */
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function ProtectedRoute({ children }) {
-  return children;
+  const navigate = useNavigate();
+  //  1.load the Authenticated user
+
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 2.If there is no authenticate user  , redirect to login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 3.while loading , show a spinner
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />;
+      </FullPage>
+    );
+
+  // 4.if there is user , render the app
+
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
