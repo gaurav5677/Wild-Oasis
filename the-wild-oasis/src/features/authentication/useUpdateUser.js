b5569cb5d39bcc3efe552ead35cce993314252eb
Gaurav <gauravpatilfamily7@gmail.com>
2023-09-12 08:27:51 +0530

    DarkMode Implemented

--- the-wild-oasis/src/features/authentication/useUpdateUser.js
+++ the-wild-oasis/src/features/authentication/useUpdateUser.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "react-hot-toast";
+import { toast } from "react-toastify";
 import { updateCurrentUser } from "../../services/apiAuth";
 
 export function useUpdateUser() {
